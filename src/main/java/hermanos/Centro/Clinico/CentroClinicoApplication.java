3019a996d72555da565ecd1136c9732b32862a08
ognjengaric <40010757+ognjengaric@users.noreply.github.com>
2019-11-08 17:42:08 +0100

    Initial project structure

---
+++ src/main/java/hermanos/Centro/Clinico/CentroClinicoApplication.java
@@ -0,0 +1,13 @@
+package hermanos.Centro.Clinico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroClinicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroClinicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hermanos/Centro/Clinico/CentroClinicoApplicationTests.java
@@ -0,0 +1,13 @@
+package hermanos.Centro.Clinico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroClinicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
