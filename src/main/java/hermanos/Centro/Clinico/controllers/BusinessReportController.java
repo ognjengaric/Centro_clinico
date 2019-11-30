08f5ef15d640d6f337da961844564a7f1ad5643a
lukac <alecar97@gmail.com>
2019-11-30 12:17:02 +0100

    Corrected mistakes in BusinessReportController

--- src/main/java/hermanos/Centro/Clinico/controllers/BusinessReportController.java
+++ src/main/java/hermanos/Centro/Clinico/controllers/BusinessReportController.java
@@ -2,42 +2,22 @@ package hermanos.Centro.Clinico.controllers;
 
 
 import hermanos.Centro.Clinico.model.Clinic;
-import hermanos.Centro.Clinico.model.ClinicAdministrator;
-import hermanos.Centro.Clinico.model.Person;
-import hermanos.Centro.Clinico.service.interfaces.ClinicAdministratorServiceInterface;
 import hermanos.Centro.Clinico.service.interfaces.ClinicServiceInterface;
-import hermanos.Centro.Clinico.service.interfaces.PersonServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping(value = "")
+@RequestMapping(value = "/viewBusinessReport")
 public class BusinessReportController {
-    @Autowired
-    ClinicAdministratorServiceInterface clinicAdminService;
-
     @Autowired
     ClinicServiceInterface clinicService;
 
-    @RequestMapping(method = RequestMethod.GET,path = "/viewBusinessReport")
-    public ArrayList<?> businessReport(@RequestBody Person person){
-
-        ClinicAdministrator admin = clinicAdminService.findByiD(person.getId());
-
-        Clinic clinic = clinicService.findById(admin.getClinic().getId());
-
-        ArrayList<String> data = new ArrayList<>();
-
-        // data = clinic.getBusinessReport();
-
-        return data;
-
+    @RequestMapping(method = RequestMethod.GET,path = "/{id}")
+    public Clinic businessReport(@PathVariable("id") long id){
+        Clinic clinic = clinicService.findById(id);
+        return clinic;
     }
 }
