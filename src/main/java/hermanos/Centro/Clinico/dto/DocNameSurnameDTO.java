f8dc5ccc280f0572a656f9f0139dcf984853c77c
lukac <alecar97@gmail.com>
2019-12-03 07:32:18 +0100

    Added DTI classes for CheckupDate, CheckupType, Room, Doctor and Doctor Rating(DocRating).

---
+++ src/main/java/hermanos/Centro/Clinico/dto/DocNameSurnameDTO.java
@@ -0,0 +1,36 @@
+package hermanos.Centro.Clinico.dto;
+
+
+public class DocNameSurnameDTO {
+    private long id;
+    private String name;
+    private String surname;
+
+    public DocNameSurnameDTO() {
+        super();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
