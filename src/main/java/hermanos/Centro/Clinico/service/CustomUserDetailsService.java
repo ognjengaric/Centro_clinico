8284833d553cffb8a13ade1470a21cce11d98816
ognjengaric <40010757+ognjengaric@users.noreply.github.com>
2019-12-04 13:20:10 +0100

    change password method in service

--- src/main/java/hermanos/Centro/Clinico/service/CustomUserDetailsService.java
+++ src/main/java/hermanos/Centro/Clinico/service/CustomUserDetailsService.java
@@ -3,9 +3,14 @@ package hermanos.Centro.Clinico.service;
 import hermanos.Centro.Clinico.model.Person;
 import hermanos.Centro.Clinico.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +19,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     PersonRepository personRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Person person = personRepository.findByEmail(email);
@@ -21,7 +32,27 @@ public class CustomUserDetailsService implements UserDetailsService {
         if(person == null)
             throw  new UsernameNotFoundException(String.format("No user found with email '%s'.", email));
 
-        return  person;
+        return person;
     }
+
+    public void changePassword(String oldPassword, String newPassword) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+
+        if (authenticationManager != null) {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, oldPassword));
+        } else {
+            return;
+        }
+
+        Person person = (Person) loadUserByUsername(email);
+
+        person.setPassword(passwordEncoder.encode(newPassword));
+        personRepository.save(person);
+
+    }
+
+
 
 }
