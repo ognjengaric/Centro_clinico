8c210fcb0f5eb2c26f628ed93307706f8bc24e96
ognjengaric <40010757+ognjengaric@users.noreply.github.com>
2020-02-02 20:38:08 +0100

    added patient to checkup

--- src/main/java/hermanos/Centro/Clinico/model/Patient.java
+++ src/main/java/hermanos/Centro/Clinico/model/Patient.java
@@ -1,15 +1,19 @@
 package hermanos.Centro.Clinico.model;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue(value = "PATIENT")
 public class Patient extends Person implements Serializable {
     @Column(nullable = true, unique = true)
     private String socialSecurityNumber;
+
+    @OneToMany(mappedBy = "patient", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Checkup> checkups = new ArrayList<>();
+
     /* TODO: 11/9/2019 medical record */
 
 //    @Column(nullable = true)
@@ -45,5 +49,13 @@ public class Patient extends Person implements Serializable {
 
     public void setSocialSecurityNumber(String socialSecurityNumber) {
         this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public List<Checkup> getCheckups() {
+        return checkups;
+    }
+
+    public void setCheckups(List<Checkup> checkups) {
+        this.checkups = checkups;
     }
 }
