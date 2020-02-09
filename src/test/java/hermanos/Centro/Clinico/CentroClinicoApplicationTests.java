99b891e3e238caf31fa0b057a0ca7688a54ce09d
ognjengaric <40010757+ognjengaric@users.noreply.github.com>
2020-02-09 15:50:19 +0100

    added testing basics

--- src/test/java/hermanos/Centro/Clinico/CentroClinicoApplicationTests.java
+++ src/test/java/hermanos/Centro/Clinico/CentroClinicoApplicationTests.java
@@ -1,8 +1,11 @@
 package hermanos.Centro.Clinico;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(Suite.class)
 @SpringBootTest
 class CentroClinicoApplicationTests {
 
